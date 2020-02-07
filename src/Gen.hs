962f08271e987e853b886d38157578803901389b
Wangxy <93236565@qq.com>
2020-02-07 11:27:13 +0800

    rename import qualified alias to improve readability.

--- src/Gen.hs
+++ src/Gen.hs
@@ -2,19 +2,19 @@ module Gen(gen) where
 
 import           Control.Monad (when)
 import qualified CmdLine as CL
-import qualified GenMessage as GM
-import qualified GenDomain  as GD
-import qualified GenService as GS
-import qualified GenRoute   as GR
+import qualified GenMessage as Message
+import qualified GenDomain  as Domain
+import qualified GenService as Service
+import qualified GenRoute   as Route
 
 gen :: CL.Options -> String -> IO ()
 gen opts file = do
     when (CL.verbose opts || CL.debug opts) $ print opts
     let gens =
-            [ GM.gen
-            , GD.gen
-            , GS.gen
-            , GR.gen
+            [ Message.gen
+            , Domain.gen
+            , Service.gen
+            , Route.gen
             ]
     mapM_ (\ g -> g opts file) gens
 
